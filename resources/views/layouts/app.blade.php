d3829c6650fa859576f1ac62a4177c3195ab97c6
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-17 09:48:47 +0100

    modify layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Scripts Braintree -->
     <script src="https://js.braintreegateway.com/web/dropin/1.33.7/js/dropin.min.js"></script>
     <script src="http://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
@@ -32,7 +32,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm position-fixed mb-5" style="top: 0; width:100%;z-index:100;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Bdoctors
@@ -53,24 +53,40 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown d-flex align-items-center justify-content-center">
+                                @foreach (Auth::user()->sponsorships as $sponsor)
+                                    <span title="Sponsorship Activate" style="height: 25px; width:25px;" class="list-unstyled rounded-circle bg-success text-light d-flex align-items-center justify-content-center"><i class="fa-solid fa-check"></i> </span>
+                                @endforeach
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{route('admin.edit')}}" class="dropdown-item">Modifica Profilo</a>
+                                    <a href="{{route('admin.sponsorship.index', [Auth::user()->name])}}" class="dropdown-item">Sponsorizza Profilo</a>
+                                    <a class="dropdown-item" href="{{ route('admin.destroy') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('delete-form').submit();">
+                                        {{ __('Elimina Profilo') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
+                                    <form id="delete-form" class="" action="{{route('admin.destroy')}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
@@ -84,9 +100,10 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="pb-4 pt-5 mt-4">
             @yield('content')
         </main>
     </div>
+    @yield('scripts')
 </body>
 </html>
