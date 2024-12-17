ad24e6a672035fb943900fdbbaeac91bbb8f7187
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-17 09:50:18 +0100

    add component.messages and modify layout

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,78 +1,114 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/home_admin.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/home_admin.css') }}">
 @endsection
 
 @section('content')
     <div class="container">
-        @if(session('success'))
-            <div class="container">
-                <div class="alert alert-success">
-                    <i class="fa-solid  fa-circle-check"></i> {{ session('success') }} 
-                </div>
+        @include('components.messages')
+
+        <div class="d-flex align-items-center justify-content-center mt-3">
+            <!-- Mostra sempre la foto del profilo -->
+            <div class="d-flex align-items-center justify-content-center mt-3">
+                @foreach (Auth::user()->sponsorships as $sponsorship)
+                    @if ($sponsorship->name == 'Base')
+                        <div class="eb_square_sponsor_base rounded-circle">
+                    @elseif ($sponsorship->name == 'Medium')
+                        <div class="eb_square_sponsor_medium rounded-circle">
+                    @elseif ($sponsorship->name == 'Pro')
+                        <div class="eb_square_sponsor_pro rounded-circle">
+                    @else
+                        <div class="eb_square rounded-circle">
+                    @endif
+
+                        @if (Auth::user()->profile_pic)
+                            <img class="rounded-circle" src="{{ asset('storage/' . Auth::user()->profile_pic) }}" />
+                        @else
+                            <img class="rounded-circle" src="{{ asset('images/avatar.png') }}">
+                        @endif
+                    </div>
+
+                    @if ($sponsorship->name == 'Base')
+                        <div style="height: 3px; width:40px;" class="bg-primary"></div>
+                    @elseif ($sponsorship->name == 'Medium')
+                        <div style="height: 3px; width:40px;" class="bg-success"></div>
+                    @elseif ($sponsorship->name == 'Pro')
+                        <div style="height: 3px; width:40px; background-color: #efb810;"></div>
+                    @else
+                        <div class="d-none"></div>
+                    @endif
+
+                    <!-- Logica per la sponsorizzazione -->
+                    @if (now()->between($sponsorship->pivot->starts_at, $sponsorship->pivot->ends_at))
+                        @if ($sponsorship->name == 'Base')
+                            <div class="sponsorship-info bg-primary text-white p-3 rounded">
+                        @elseif ($sponsorship->name == 'Medium')
+                            <div class="sponsorship-info bg-success text-white p-3 rounded">
+                        @elseif ($sponsorship->name == 'Pro')
+                            <div class="sponsorship-info text-white p-3 rounded" style="background-color: #ffbf00;">
+                        @endif
+                            <p class="m-0">Livello: {{ $sponsorship->name }}</p>
+                            @php
+                                $remainingTime = now()->diffInHours($sponsorship->pivot->ends_at);
+                                $days = floor($remainingTime / 24);
+                                $hours = $remainingTime % 24;
+                                $giorno;
+                                if ($days < 2) {
+                                    $giorno = 'giorno';  # code...
+                                }else{
+                                    $giorno = 'giorni';
+                                }
+                            @endphp
+                            <p class="m-0">Tempo rimanente: {{ $days }} {{ $giorno }} e {{ $hours }} ore</p>
+                        </div>
+                    @endif
+                @endforeach
             </div>
-        @endif
-        <div class="d-flex align-items-center justify-content-center eb_square m-auto rounded-circle">
-            @if (Auth::user()->profile_pic) 
-                <img class="rounded-circle" src="{{ asset('storage/' .  Auth::user()->profile_pic) }}"/>
-            @else
-                <img class="rounded-circle" src="{{asset('images/avatar.png')}}">   
-            @endif
         </div>
-        <h1 class="text-center mt-2">Benvenuto Dr. {{Auth::user()->name}} {{Auth::user()->surname}}</h1>
-        <span class="d-block text-center"><span class="font-weight-bold">Mail:</span> {{Auth::user()->email}}</span>
+
+        <!-- Altri contenuti della pagina -->
+        <h2 class="text-center mt-2 text-dark font-weight-bold">Benvenuto Dr. {{ Auth::user()->name }} {{ Auth::user()->surname }}</h2>
         <div class="row mt-3">
-            {{-- -------------- --}}
+            <!-- Specializzazioni -->
             <div class="col-12 col-md-5 my-2 p-3 my-radius-bg">
                 <h5 class="font-weight-bold text-center">Le Tue Specializzazioni</h5>
                 <div class="d-flex flex-wrap">
                     @foreach (Auth::user()->specializations as $specialization)
-                        <span class="list-unstyled mr-3">{{ $specialization->name }} </span>
+                        <span class="list-unstyled mr-3">{{ $specialization->name }}</span>
                     @endforeach
                 </div>
             </div>
-             {{-- -------------- --}}
-            <div class="col-12 col-md-5 my-2 my-radius-bg  offset-md-2 p-3">
+            <!-- Servizi offerti -->
+            <div class="col-12 col-md-5 my-2 my-radius-bg offset-md-2 p-3">
                 @if (Auth::user()->services)
                     <h5 class="text-center font-weight-bold">Prestazioni offerte</h5>
                     <p>{{ Auth::user()->services }}</p>
                 @endif
             </div>
         </div>
-         {{-- -------------- --}}
         <div class="row">
-            <div class="col-12 col-md-5 my-2 my-radius-bg  p-3">
+            <div class="col-12 col-md-5 my-2 my-radius-bg p-3">
                 <div class="text-center">
                     <h5 class="mb-4">Hai ricevuto {{ count(Auth::user()->reviews) }} recensioni</h5>
-                    <a href="{{route('admin.reviews.index', [Auth::user()->name])}}" class="btn eb_btn">Vedi Recensioni</a>
+                    <a href="{{ route('admin.reviews.index', [Auth::user()->name]) }}" class="btn eb_btn">Vedi Recensioni</a>
                 </div>
             </div>
-            <div class="col-12 col-md-5 offset-md-2 my-2 my-radius-bg  p-3">
+            <div class="col-12 col-md-5 offset-md-2 my-2 my-radius-bg p-3">
                 <div class="text-center">
                     <h5 class="mb-4">Hai ricevuto {{ count(Auth::user()->messages) }} messaggi</h5>
-                    <a href="{{route('admin.messages.index', [Auth::user()->name])}}" class="btn eb_btn">Vedi Messaggi</a>
+                    <a href="{{ route('admin.messages.index', [Auth::user()->name]) }}" class="btn eb_btn">Vedi Messaggi</a>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-12 my-2 my-radius-bg p-3 text-center"> 
-                @if ( Auth::user()->cv)
-                <a class="btn eb_btn mt-3" href="{{ asset('storage/' .  Auth::user()->cv) }}" download>Download CV</a>
+            <div class="col-12 my-2 my-radius-bg p-3 text-center">
+                @if (Auth::user()->cv)
+                    <a class="btn eb_btn mt-3" href="{{ asset('storage/' . Auth::user()->cv) }}" download>Download CV</a>
                 @else
-                <h5 class="pt-2 font-weight-bold">Nessun CV caricato!</h5>  
+                    <h5 class="pt-2 font-weight-bold">Nessun CV caricato!</h5>
                 @endif
             </div>
         </div>
-        <div class="mt-3 text-center">
-            <a href="{{route('admin.edit')}}" class="btn eb_btn mx-3">Modifica Profilo</a>
-            <a href="{{route('admin.sponsorship.index', [Auth::user()->name])}}" class="btn btn-success mx-3">Sponsorizza Profilo</a>
-           <form class="d-inline-block mx-3" action="{{route('admin.destroy')}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Elimina Profilo</button>
-            </form>
-        </div> 
     </div>
- 
 @endsection
